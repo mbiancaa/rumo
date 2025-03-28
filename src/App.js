f036d0c9d8abde82f55da7538ac317d7bdd935e1
Bianca <mbiancaadr@gmail.com>
2025-03-28 12:48:48 +0200

    redirects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
